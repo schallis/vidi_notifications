fcfc4ad28abacd8f205baa375c216a8517c54e48
Thomas Grainger <thomasgrainger@graingert.co.uk>
2014-10-06 11:38:03 +0100

    Preparing release 1.0.0

--- HISTORY.rst
+++ HISTORY.rst
@@ -4,7 +4,13 @@ History
 -------
 
 
-0.1.0 (unreleased)
+1.0.0 (2014-10-06)
+++++++++++++++++++
+
+- Support django 1.6.7
+
+
+0.1.0 (unknown)
 ++++++++++++++++++
 
 * First release on PyPI.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vidi-notifications",
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages('src', exclude=('tests',)),
     package_dir={'': 'src'},
     include_package_data=True,
