8fe7cb8d5c2e14c6fff2451dddd16d012e79ec05
Chris D'cunha <Chris.D'cunha@hogarthww.com>
2015-04-20 21:01:50 +0100

    Back to development: 2.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vidi-notifications",
-    version='2.2.0',
+    version='2.2.1.dev0',
     packages=find_packages('src', exclude=('tests',)),
     package_dir={'': 'src'},
     include_package_data=True,
